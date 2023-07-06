5957e4c7def1e47e14500f37514b4736b1789dd1
AlifJamain <67289330+AlifJamain@users.noreply.github.com>
2023-07-06 13:45:45 +0800

    Update index.php

--- endpoint/index.php
+++ endpoint/index.php
@@ -1,1 +1,10 @@
+<?php
+require('vendor/autoload.php');
 
+use NoahBuscher\Macaw\Macaw;
+
+Macaw::get('/api/method-get', 'application\controllers\apiController@method_get');
+Macaw::post('/api/method-post', 'application\controllers\apiController@method_post');
+
+Macaw::dispatch();
+?>
